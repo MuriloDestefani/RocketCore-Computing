a79271ecfe1897869bdc7338fae2bf38ff98c6e4
MuriloDestefani <murilo_destefani@hotmail.com>
2023-04-06 11:46:41 -0400

    update 7 files

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const express = require('express');
 const app = express();
 const hbs = require('express-handlebars');
 const PORT = process.env.PORT || 3000;
-// Configuracao do HandleBars
+// Configuracao do HandleBars
 app.engine('hbs',hbs.engine({
     extname: 'hbs', 
     defaultLayout: 'main'
     })); 
 app.set('view engine', 'hbs');
-// rota inicial 
+// rota inicial 
 // renderiza o home.hbs para abra dentro da tag {{{body}}} no layout
 app.get('/',(req,res)=>{
 res.render('home'); 
 });
-//ativar o sistema
+//ativar o sistema
 app.listen(PORT,()=>{
  console.log('Servidor rodando em http://localhost:' +PORT);
 })
@@ -25,7 +25,7 @@ app.use(express.static('public'));
 app.get('/cad_users',(req,res)=>{
 res.render('cad_users'); 
 });
-// rota renderizada 
+// rota renderizada 
 app.get('/exibir_users',(req,res)=>{
 res.render('exibir_users'); 
 });
@@ -33,5 +33,10 @@ res.render('exibir_users');
 // rota renderizada 
 app.get('/editar_users',(req,res)=>{
 res.render('editar_users'); 
+});
+
+// rota renderizada 
+app.get('/prods',(req,res)=>{
+res.render('prods'); 
 });
     
